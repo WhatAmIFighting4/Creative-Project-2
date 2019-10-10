d505dc2590bfe5bcab52be1015111f22c357d88d
WhatAmIFighting4 <50183259+WhatAmIFighting4@users.noreply.github.com>
2019-10-09 20:33:52 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 
+/*global fetch*/
+/*global moment*/
+
+const url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=DOBNGR12V9DJ5DIL";
+fetch(url)
+    .then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        console.log(json);
+        let results = "";
+        results += '<h2>' + json.query + "</h2>";
+        document.getElementById("dailyStock").innerHTML = results;
+    });
+        /*results += '<h2>' + json.dailyData + " &deg;F</h2>"
+        results += "<p>"
+        /*for (let i = 0; i < json.weather.length; i++) {
+            results += json.weather[i].description
+            if (i !== json.weather.length - 1)
+                results += ", "
+        }
+/*results += "</p>";
+            document.getElementById("weatherResults").innerHTML = results;
+        });*/
